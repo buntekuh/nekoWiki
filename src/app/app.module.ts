d07fcca683d433d8383d99d6eef925184768c33a
buntekuh <me@mymonster.net>
2017-04-04 16:19:32 +0200

    page component, admin component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { Routes, RouterModule } from "@angular/router";
+import { AdminComponent } from './admin/admin.component';
+import { PageComponent } from './page/page.component';
+
+const appRoutes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: '', component: PageComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    AdminComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
